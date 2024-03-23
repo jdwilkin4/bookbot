0fc2247427d2bae4a5b7813b82af0d655c87aa76
jdwilkin4 <jwilkin4@hotmail.com>
2024-03-22 23:07:19 -0700

    feat: adding get letters function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     path = 'books/frankenstein.txt'
     book_contents = get_book(path)
     word_count = get_words(book_contents)
-    print(word_count)
+    letter_count = get_letters(book_contents)
+    print(letter_count)
     
 def get_book(path):
     with open(path) as f:
@@ -10,6 +11,19 @@ def get_book(path):
     
 def get_words(book):
     return len(book.split())
+
+def get_letters(book):
+    dict = {}
+    
+    for char in book:
+        lowercase_char = char.lower()
+        if lowercase_char.isalpha():
+            if lowercase_char in dict:
+                dict[lowercase_char] += 1
+            else:
+                dict[lowercase_char] = 1
+    return dict
+            
         
 if __name__ == "__main__":
     main()
