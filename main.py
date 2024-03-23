4ac6e8447ed343bec632bf293a01cdab1c8b27c5
jdwilkin4 <jwilkin4@hotmail.com>
2024-03-23 00:03:15 -0700

    feat: add print report function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     book_contents = get_book(path)
     word_count = get_words(book_contents)
     letter_count = get_letters(book_contents)
-    print(letter_count)
+    list_of_letter_dicts = get_chars_list(letter_count)
+    print_report(path,word_count,list_of_letter_dicts)
     
 def get_book(path):
     with open(path) as f:
@@ -23,7 +24,31 @@ def get_letters(book):
             else:
                 dict[lowercase_char] = 1
     return dict
-            
+
+def get_chars_list(letters):
+    chars_list = []
+    for char in letters:
+        dict = {}
+        dict[char] = letters[char]
+        chars_list.append(dict)
+        
+    return chars_list
+
+def sort_by(dict):
+    return dict[list(dict.keys())[0]]
+
+def print_report(path, words, letter_list):
+    letter_list.sort(reverse=True, key=sort_by)
+    
+    print(f'--- Begin report of {path} ---')
+    print(f'{words} words found in the document')
+    print("\n")
+    
+    for dict in letter_list:
+        for char, num in dict.items():
+            print(f'The \'{char}\' character was found {num} times')
+    
+    print('--- End report ---')      
         
 if __name__ == "__main__":
     main()
